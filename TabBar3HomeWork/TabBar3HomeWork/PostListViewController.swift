221228a216b9f9054b0d7260ae8b2db43969efaa
İbrahim Ballıbaba <w4nderlust3rr@gmail.com>
2022-09-21 22:29:05 +0300

    first init
    first init

---
+++ TabBar3HomeWork/TabBar3HomeWork/PostListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TabBar3HomeWork
+//
+//  Created by İbrahim Ballıbaba on 21.09.2022.
+//
+
+import UIKit
+
+class PostListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
